216346c9bdc89b64eefd5cdcfbc5cf5512c0d1cb
Amar2050 <amarboutayeb@gmail.com>
2019-08-08 21:04:37 +0200

    Section 4

--- symbnb/src/Controller/HomeController.php
+++ symbnb/src/Controller/HomeController.php
@@ -4,28 +4,10 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
-class HomeController extends Controller {
-    /**
-     * 
-     * @Route("/hello/{prenom}/age/{age}", name="hello")
-     * @Route("/hello", name= "hello_base")
-     * @Route("/hello/{prenom}", name= "hello_prenom")
-     * Montre une page qui dit bonjour
-     * 
-     * @return void
-     */
-    public function hello($prenom = "anonyme", $age = 0){
-        return $this->render(
-            'hello.html.twig',
-            [
-                'prenom'    => $prenom,
-                'age'       => $age
-            ]
-        );
-    }
+class HomeController extends AbstractController {
 
     /**
      * Lead to home page
@@ -35,14 +17,8 @@ class HomeController extends Controller {
      * @return void
      */
     public function home(){
-    $prenoms = ["Amar" => '31', "Ali" => '10', "Amir" => '12', "Ahmed" => '14'];
         return $this->render(
-            'home.html.twig', 
-            [
-                'title'     => 'Bonjour à tous !',
-                'age'       => '18',
-                'tableau'   => $prenoms
-            ]
+            'home.html.twig'
         );
     }
 }
