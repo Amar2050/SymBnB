ef4a44c4df46971bd17e3f6c785b0b95795889da
Amar2050 <amarboutayeb@gmail.com>
2019-08-15 02:15:33 +0200

    Still in progress

--- symbnb/src/Controller/AdController.php
+++ symbnb/src/Controller/AdController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Form\AdType;
 use App\Repository\AdRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdController extends AbstractController
@@ -31,7 +33,14 @@ class AdController extends AbstractController
          */
     
         public function create(){
-            return $this->render('ad/new.html.twig');
+            $ad = new Ad;
+
+            $form = $this->createForm(AdType::class);
+
+            return $this->render('ad/new.html.twig',  [
+                'form' => $form->createView()
+
+            ]);
         }
 
     /**
