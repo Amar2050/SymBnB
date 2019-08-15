62ef31c3d2b82488684f8641a7b8dcb32f576b13
Amar2050 <amarboutayeb@gmail.com>
2019-08-15 17:57:00 +0200

    Section 5 done

--- symbnb/src/Controller/AdController.php
+++ symbnb/src/Controller/AdController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Ad;
 use App\Form\AdType;
 use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -32,10 +34,29 @@ class AdController extends AbstractController
          * @return Response
          */
     
-        public function create(){
+        public function create(Request $request, ObjectManager $manager){
             $ad = new Ad;
 
-            $form = $this->createForm(AdType::class);
+            $form = $this->createForm(AdType::class, $ad);
+
+            $form->handleRequest($request);
+            
+
+            if ($form->isSubmitted() && $form->isValid()) {
+
+                $manager->persist($ad);
+                $manager->flush();
+
+                $this->addFlash(
+                    'success' ,
+                    "L'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée !"
+                );
+                
+                return $this->redirectToRoute('ad_show', [
+                    'slug' => $ad->getSlug()
+                ]);
+
+            }
 
             return $this->render('ad/new.html.twig',  [
                 'form' => $form->createView()
--- symbnb/src/Form/AdType.php
+++ symbnb/src/Form/AdType.php
@@ -6,19 +6,56 @@ use App\Entity\Ad;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class AdType extends AbstractType
 {
+
+    /**
+     * Give basic field config 
+     *
+     * @param string $label
+     * @param string $placeholder
+     * @return array
+     */
+    private function getConfiguration ($label, $placeholder){
+        return [
+            'label'         => $label ,
+            'attr'           => [
+                'placeholder'   => $placeholder 
+            ]
+        ];
+
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('slug')
-            ->add('price')
-            ->add('introduction')
-            ->add('content')
-            ->add('coverImage')
-            ->add('rooms')
+            ->add('title', TextType::class , 
+            $this->getConfiguration("Titre", "Renseignez un titre pour votre annonce") )
+
+            ->add('slug' , TextType::class ,
+            $this->getConfiguration("Adresse web", "Renseignez une adresse web (automatique)") )
+
+            ->add('coverImage', UrlType::class,
+            $this->getConfiguration("URL de l'image principale", "Envoyez votre meilleur image !") )
+            
+            ->add('introduction', TextType::class,
+            $this->getConfiguration("Introduction", "Tapez une introduction pour votre annonce") )
+            
+            ->add('content', TextareaType::class,
+            $this->getConfiguration("Déscriptif", "Donnez nous envie de venir chez vous !") )
+            
+            ->add('price', MoneyType::class,
+            $this->getConfiguration("Prix par nuit", "Renseignez un prix par nuit") )
+            
+            ->add('rooms', IntegerType::class,
+             $this->getConfiguration("Chambres", "Renseignez votre nombre de chambres") ) 
+
         ;
     }
 
