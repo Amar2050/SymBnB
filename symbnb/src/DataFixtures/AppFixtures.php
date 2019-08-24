9e43322e654e93610b0d10ee892dd2218dcdcc65
Amar2050 <amarboutayeb@gmail.com>
2019-08-24 23:33:37 +0200

    Section 9
    creating Role entity
    begining of role user

--- symbnb/src/DataFixtures/AppFixtures.php
+++ symbnb/src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use Faker\Factory;
+use App\Entity\Role;
 use App\Entity\User;
 use App\Entity\Image;
 use Cocur\Slugify\Slugify;
@@ -21,6 +22,21 @@ class AppFixtures extends Fixture
     {
         $faker      = Factory::create('fr_FR');
 
+        $adminRole = new Role();
+        $adminRole->setTitle('ROLE_ADMIN');
+        $manager->persist($adminRole);
+
+        $adminUser = new User();
+        $adminUser->setFirstName('Amar')
+                  ->setLastName('Bou')
+                  ->setEmail('amar2050@hotmail.fr')
+                  ->setHash($this->encoder->encodePassword($adminUser, 'password'))
+                  ->setPicture('https://www.gravatar.com/avatar/00000000000000000000000000000000?d=wavatar&f=y')
+                  ->setIntroduction('Moi un User pas comme les autres')
+                  ->setDescription('<p>'.join('</p><p>', $faker->paragraphs(3)).'</p>')
+                  ->addUserRole($adminRole);
+        $manager->persist($adminUser);
+        
         //Creating user
         $users = [];
         $genres = ['male', 'female'];
