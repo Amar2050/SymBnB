4ec01833f462a94d13d6b5c128bfbd23080d51ec
Amar2050 <amarboutayeb@gmail.com>
2019-09-18 15:39:31 +0200

    Moderate an ad
    creating function in controller who permit to moderate an ad
    still some stuff to do but working for the moment

--- symbnb/src/Controller/AdminAdController.php
+++ symbnb/src/Controller/AdminAdController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
+use App\Form\AdType;
 use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -15,6 +19,35 @@ class AdminAdController extends AbstractController
     {
         return $this->render('admin/ad/index.html.twig', [
             'ads' => $repo->findAll(),
+        ]);
+    }
+
+    /**
+     * Admin edit function 
+     *
+     * @Route("/admin/ads/{id}/edit" , name="admin_ads_edit")
+     * @param Ad $ad
+     * @return Response
+     */
+    public function edit(Ad $ad, Request $request, ObjectManager $manager){
+        $form= $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été modifié !"
+            );
+        }
+
+        return $this->render('admin/ad/edit.html.twig', [
+            'ad'   => $ad,
+            'form' => $form->createView()
+
         ]);
     }
 }
